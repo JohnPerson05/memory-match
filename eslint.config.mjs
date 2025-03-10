2a8dd5896bc1cc0f29e67ed568f9b4b4eac72838
JohnPerson05 <jpnarral@gmail.com>
2025-03-10 21:00:53 +0800

    chore: update eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { varsIgnorePattern: "^_" }, // Ignore unused variables starting with `_`
+      ],
+      "react-hooks/exhaustive-deps": "warn", // Convert missing dependency errors to warnings
+    },
+  },
 ];
 
 export default eslintConfig;
